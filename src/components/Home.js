18a8a76de6d1dc73bdb46e51c85e12031cf86a2f
Vinh <vinhbuith32412000@gmail.com>
2022-06-24 09:50:19 +0700

    Fix bugs routing and className

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom"
 const Home = () => {
   return(
     <div className="Home">
-      <div class="message">
+      <div className="message">
         <h1>Welcome to our shop!</h1>
         <h2>We sell glasses and hats from Revachol</h2>
         <Link 
           to="/shop"
           style={{textDecoration: "none"}}>
-          <div class="link-to-shop">Go shopping now</div>
+          <div className="link-to-shop">Go shopping now</div>
         </Link>
       </div>
     </div>
